d02f946e3ea7520c1ac6c9e0c964cba3b5dc4575
Dalton <daltongetbaker@gmail.com>
2024-03-27 18:08:25 -0400

    products.js fixed post issue

--- routes/products.js
+++ routes/products.js
@@ -2,51 +2,37 @@ var express = require("express");
 var router = express.Router();
 var mongoose = require("mongoose");
 
-const Product = require('../models/Product')
-// const Task = require("../models/Task");
+const Product = require("../models/Product");
+const isAuthenticated = require("../middleware/isAuthenticated");
+const isOwner = require("../middleware/isOwner");
 
-const isAuthenticated = require('../middleware/isAuthenticated')
-const isOwner = require('../middleware/isOwner')
+router.post("/", isAuthenticated, (req, res, next) => {
+  const {title, description, price, rating, stock, brand, category, thumbnail, images,} = req.body;
 
-router.post("/products", isAuthenticated, (req, res, next) => {
-    const { title, description, price, rating, stock, brand, category, thumbnail, images } = req.body;
-  
-    Product.create({
-      title,
-      description,
-      price,
-      rating,
-      stock,
-      brand,
-      category,
-      thumbnail,
-      images,
-      owner: req.user._id
+  Product.create({
+    title,
+    description,
+    price,
+    rating,
+    stock,
+    brand,
+    category,
+    thumbnail,
+    images,
+    owner: req.user._id,
+  })
+    .then((createdProduct) => {
+      console.log("this is the created product ===>", createdProduct);
+      res.json(createdProduct);
     })
-      .then((createdProduct) => {
-        console.log("this is the created project ===>", createdProduct);
-        res.json(createdProduct);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.json(err);
-      });
-  });
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
 
-  router.get("/", (req, res) => {
-    Product.find({})
-      .then((products) => {
-        console.log("Retrieved cohorts ->", products);
-        res.json(products);
-      })
-      .catch((error) => {
-        console.error("Error while retrieving cohorts ->", error);
-        res.status(500).json({ errorMsg: "Failed to retrieve cohorts", error });
-      });
-  });
+module.exports = router;
 
-  module.exports = router;
-  
 //   title: String,
 //   description: String,
 //   price: Number,

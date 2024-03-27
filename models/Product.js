2bcf492e42d250c26f558b17e3220aa2a0de206c
Dalton <daltongetbaker@gmail.com>
2024-03-27 15:12:07 -0400

    Product.js first add

---
+++ models/Product.js
@@ -0,0 +1,23 @@
+const { model, Schema } = require("mongoose");
+const productSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    price: Number,
+    rating: Number,
+    stock: Number,
+    brand: String,
+    category: String,
+    thumbnail: String,
+    images: Array,
+    owner: {type: Schema.Types.ObjectId, ref: 'User'}
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Student = mongoose.model("Product", productSchema);
+
+
+module.exports = model("Product", productSchema);

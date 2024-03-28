a3a7607c00bafecf1b29fc5ba432b0d6f90af8b1
Dalton <daltongetbaker@gmail.com>
2024-03-28 19:29:15 -0400

    user.create comment for type admin

--- routes/auth.js
+++ routes/auth.js
@@ -53,6 +53,7 @@ router.post("/signup", (req, res, next) => {
       .then((createdUser) => {
         // Deconstruct the newly created user object to omit the password
         // We should never expose passwords publicly
+        //Added type for the admin and user
         const { email, name, _id, type } = createdUser;
 
         // Create a new object that doesn't expose the password

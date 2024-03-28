3943c7858eef96a9a0435915b4312c8f1c4e4578
Dalton <daltongetbaker@gmail.com>
2024-03-28 19:09:13 -0400

    added admin to User.js

--- models/User.js
+++ models/User.js
@@ -15,6 +15,11 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+
+    type: {
+      type: String, enum: [ "Admin", "User"],
+      default: "User"
+    }
   },
   {
     timestamps: true,

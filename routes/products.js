2378cfbe2b8a4e5b544a1fa36cd03b6d4566b221
Dalton <daltongetbaker@gmail.com>
2024-03-29 20:23:58 -0400

    updated product.js friday

--- routes/products.js
+++ routes/products.js
@@ -22,14 +22,14 @@ router.post("/", isAuthenticated, isAdmin, (req, res, next) => {
     images,
     owner: req.user._id,
   })
-    .then((createdProduct) => {
-      console.log("this is the created product ===>", createdProduct);
-      res.json(createdProduct);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json(err);
-    });
+  .then((createdProduct) => {
+    console.log("this is the created product ===>", createdProduct);
+    res.json(createdProduct);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.json(err);
+  });
 });
 
 // Get all products
@@ -55,18 +55,18 @@ router.get("/:productId", (req, res, next) => {
   .catch((error) => {
     console.error("Failed to retrieve product", error);
     res.json({ errorMsg: "Failed to retrieve product", error});
-  });
+  });                                           
 });
 
 // Update product ID with Admin
 router.put("/update/:productId", isAuthenticated, isAdmin, (req, res, next) => {
   const { productId } = req.params;
-
+  
   if (!mongoose.Types.ObjectId.isValid(productId)) {
     res.status(400).json({ message: "Specified is is not valid" });
     return;
   }
-
+  
   Product.findByIdAndUpdate(productId, req.body, { new: true })
   .then((updatedProduct) => {
     console.log("Updated product ====>", updatedProduct);
@@ -80,14 +80,14 @@ router.put("/update/:productId", isAuthenticated, isAdmin, (req, res, next) => {
 
 // Delete product by ID with Admin
 router.delete("/delete/:productId", isAuthenticated, isAdmin, (req, res, next) => {
-
+  
   const { productId } = req.params;
-
+  
   if (!mongoose.Types.ObjectId.isValid(productId)) {
     res.status(400).json({ message: "Specified is is not valid" });
     return;
   }
-
+  
   Product.findByIdAndDelete(productId)
   .then((deletedProduct) => {
     console.log("This is our deleted product", deletedProduct)
@@ -97,7 +97,117 @@ router.delete("/delete/:productId", isAuthenticated, isAdmin, (req, res, next) =
     console.log(err);
     res.json(err);
   });
-
+  
 })
 
 module.exports = router;
+
+
+
+
+
+
+
+// var express = require("express");
+// var router = express.Router();
+// var mongoose = require("mongoose");
+
+// const Product = require("../models/Product");
+// const isAuthenticated = require("../middleware/isAuthenticated");
+// const isAdmin = require("../middleware/isAdmin")
+
+// // Create new product only for Admin
+// router.post("/", isAuthenticated, isAdmin, (req, res, next) => {
+//   const {title, description, price, rating, stock, brand, category, thumbnail, images,} = req.body;
+
+//   Product.create({
+//     title,
+//     description,
+//     price,
+//     rating,
+//     stock,
+//     brand,
+//     category,
+//     thumbnail,
+//     images,
+//     owner: req.user._id,
+//   })
+//     .then((createdProduct) => {
+//       console.log("this is the created product ===>", createdProduct);
+//       res.json(createdProduct);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.json(err);
+//     });
+// });
+
+// // Get all products
+// router.get("/", (req, res, next) => {
+//   Product.find()
+//   .then((foundProducts) => {
+//     console.log("Found Product ===>", foundProducts);
+//     res.json(foundProducts)
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     res.json(err)
+//   });
+// });
+
+// // Get product by ID
+// router.get("/:productId", (req, res, next) => {
+//   Product.findById(req.params.productId)
+//   .then((foundProducts) => {
+//     console.log("Found product ->", foundProducts);
+//     res.json(foundProducts)
+//   })
+//   .catch((error) => {
+//     console.error("Failed to retrieve product", error);
+//     res.json({ errorMsg: "Failed to retrieve product", error});
+//   });
+// });
+
+// // Update product ID with Admin
+// router.put("/update/:productId", isAuthenticated, isAdmin, (req, res, next) => {
+//   const { productId } = req.params;
+
+//   if (!mongoose.Types.ObjectId.isValid(productId)) {
+//     res.status(400).json({ message: "Specified is is not valid" });
+//     return;
+//   }
+
+//   Product.findByIdAndUpdate(productId, req.body, { new: true })
+//   .then((updatedProduct) => {
+//     console.log("Updated product ====>", updatedProduct);
+//     res.json(updatedProduct);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     res.json(err);
+//   });
+// });
+
+// // Delete product by ID with Admin
+// router.delete("/delete/:productId", isAuthenticated, isAdmin, (req, res, next) => {
+
+//   const { productId } = req.params;
+
+//   if (!mongoose.Types.ObjectId.isValid(productId)) {
+//     res.status(400).json({ message: "Specified is is not valid" });
+//     return;
+//   }
+
+//   Product.findByIdAndDelete(productId)
+//   .then((deletedProduct) => {
+//     console.log("This is our deleted product", deletedProduct)
+//     res.json(deletedProduct)
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     res.json(err);
+//   });
+
+// })
+
+// module.exports = router;

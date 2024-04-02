242e08affb91605a72533ab19ee67cfbed2ee707
Dalton <daltongetbaker@gmail.com>
2024-04-02 12:49:04 -0400

    tried to make small changes

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// router.get("/", (req, res, next) => {
+//   User.find()
+//   .then((foundUsers) => {
+//     console.log("Found User ===>", foundUsers);
+//     res.json(foundUsers)
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     res.json(err)
+//   });
+// });
+
 module.exports = router;
